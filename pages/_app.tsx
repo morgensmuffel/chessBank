6cb2a5f537c5286403a6d977cf2ba3f4158e4678
Mariia M <blucktea@gmail.com>
2022-11-30 20:52:33 -0500

    Limits Update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>ChessBank</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="crossOrigin" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+// const roboto = Roboto({
+//   weight: '400',
+//   subsets: ['latin'],
+// })
+
+
